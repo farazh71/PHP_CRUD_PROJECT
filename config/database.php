be831c28d83e95c09fbd42e89a1db605d92d7d59
Faraz Hussain <faraz234hussain@gmail.com>
2024-07-05 23:10:35 +0530

    Changing database

--- config/database.php
+++ config/database.php
@@ -10,7 +10,7 @@ session_start();
 $servername="localhost";
 $username="root";
 $password="";
-$dbName="temp";
+$dbName="mydb";
 $conn=new mysqli($servername,$username,$password,$dbName);
 // if($conn->connect_error){
 //     die("Connection failed: ".$conn->connect_error);
